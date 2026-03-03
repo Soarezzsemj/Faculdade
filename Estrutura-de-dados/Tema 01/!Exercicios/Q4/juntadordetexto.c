c5ceac23ba1b40b2224d7fc19d6225e0fc49436a
Soarezzsemj <popcornor6topster@outlook.com>
2026-03-02 22:21:03 -0400

    .

---
+++ Estrutura-de-dados/Tema 01/!Exercicios/Q4/juntadordetexto.c
@@ -0,0 +1,28 @@
+//
+// Created by soarezz on 3/2/26.
+//
+
+// Escreva um programa que solicite do usuário os nomes de 2 arquivos texto para, a seguir, criar um
+// terceiro arquivo, também texto, com o conteúdo dos dois arquivos fornecidos (o conteúdo do
+// primeiro arquivo seguido do conteúdo do segundo arquivo).
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+
+    FILE * fp;
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
