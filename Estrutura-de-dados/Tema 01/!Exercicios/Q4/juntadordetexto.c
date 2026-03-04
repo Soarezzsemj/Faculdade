33f7980815ccf95a1d6f9bfa60235779d73ed74e
Soarezzsemj <popcornor6topster@outlook.com>
2026-03-04 01:52:03 -0300

    .

--- Estrutura-de-dados/Tema 01/!Exercicios/Q4/juntadordetexto.c
+++ Estrutura-de-dados/Tema 01/!Exercicios/Q4/juntadordetexto.c
@@ -13,16 +13,61 @@
 
 int main () {
 
-    FILE * fp;
+        char nome1[100], nome2[100], nome3[100];
+        FILE *fp1, *fp2, *fp3;
+        int c;
 
+        // Ler nomes dos arquivos
+        printf("Digite o nome do primeiro arquivo: ");
+        scanf("%s", nome1);
 
+        printf("Digite o nome do segundo arquivo: ");
+        scanf("%s", nome2);
 
+        printf("Digite o nome do terceiro arquivo (saida): ");
+        scanf("%s", nome3);
 
+        // Abrir arquivos de leitura
+        fp1 = fopen(nome1, "r");
+        if (fp1 == NULL) {
+            printf("Erro ao abrir o primeiro arquivo.\n");
+            exit(1);
+        }
 
+        fp2 = fopen(nome2, "r");
+        if (fp2 == NULL) {
+            printf("Erro ao abrir o segundo arquivo.\n");
+            fclose(fp1);
+            exit(1);
+        }
 
+        // Criar arquivo de saída
+        fp3 = fopen(nome3, "w");
+        if (fp3 == NULL) {
+            printf("Erro ao criar o terceiro arquivo.\n");
+            fclose(fp1);
+            fclose(fp2);
+            exit(1);
+        }
 
+        // Copiar conteúdo do primeiro arquivo
+        while ((c = fgetc(fp1)) != EOF) {
+            fputc(c, fp3);
+        }
 
+        // Copiar conteúdo do segundo arquivo
+        while ((c = fgetc(fp2)) != EOF) {
+            fputc(c, fp3);
+        }
 
-    return 0;
+        // Fechar arquivos
+        fclose(fp1);
+        fclose(fp2);
+        fclose(fp3);
+
+        printf("Arquivos unidos com sucesso!\n");
+
+        return 0;
+
 }
 
